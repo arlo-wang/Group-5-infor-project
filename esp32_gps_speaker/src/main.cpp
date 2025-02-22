495d940dca58b5ab21bd99b368d3037d1d2b39cb
Qichao(Arlo) Wang <arlowang23@gmail.com>
2025-02-22 19:02:39 +0000

    esp32_gps_speaker wifi update

--- esp32_gps_speaker/src/main.cpp
+++ esp32_gps_speaker/src/main.cpp
@@ -1,18 +1,49 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
-#define LED_PIN 2
+constexpr int LED_PIN = 2;
 
-void setup() {
-  Serial.begin(115200);
-  pinMode(LED_PIN, OUTPUT);
+// WiFi settings
+const char* ssid = "VM5892311";
+const char* password = "q6hQzcdmjqwq"; 
+
+
+void connectToWiFi()
+{
+    // connect to WiFi
+    Serial.printf("Connecting to %s ", ssid);
+    WiFi.begin(ssid, password);
+
+    // wait for WiFi connection
+    while (WiFi.status() != WL_CONNECTED) {
+        digitalWrite(LED_PIN, HIGH);  // LED flash to show connecting
+        delay(1000);
+        digitalWrite(LED_PIN, LOW);
+        delay(1000);
+        Serial.print("connecting...");
+    }
+    
+    // WiFi connected
+    digitalWrite(LED_PIN, HIGH);  // LED on to show connected
+    Serial.println("\nWiFi connected!");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
-void loop() {
-  Serial.println("Blinking LED...");
-  
-  digitalWrite(LED_PIN, HIGH); // LED ON
-  delay(500);
-  
-  digitalWrite(LED_PIN, LOW);  // LED OFF
-  delay(500);
+void setup() 
+{
+    Serial.begin(115200);
+    delay(1000);
+    pinMode(LED_PIN, OUTPUT);
+    connectToWiFi();
+}
+
+void loop() 
+{
+    digitalWrite(LED_PIN, HIGH);  // LED flash to show connecting
+    delay(1000);
+    digitalWrite(LED_PIN, LOW);
+    delay(1000);
+    if (WiFi.status() == WL_CONNECTED) Serial.println("WiFi connected");
+    else Serial.println("WiFi disconnected");
 }
