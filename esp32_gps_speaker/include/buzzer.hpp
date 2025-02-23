ad2260071fff4202595d0d6c7eaf0cbacee7b7c3
Qichao(Arlo) Wang <arlowang23@gmail.com>
2025-02-23 18:11:05 +0000

    Implement FreeRTOS tasks for buzzer and LED control

--- esp32_gps_speaker/include/buzzer.hpp
+++ esp32_gps_speaker/include/buzzer.hpp
@@ -6,13 +6,13 @@ namespace buzzer {
     constexpr int buzzer_PIN = 39;
 
     // local setup
-    void localSetup() {
+    inline void localSetup() {
         pinMode(buzzer_PIN, OUTPUT);
     }
 
     // local loop
-    void localLoop() {
-        for (int i = 0; i < 5; i++) {
+    inline void localLoop() {
+        for (int i = 0; i < 3; i++) {
             tone(buzzer_PIN, 4000, 500);
             // Serial.println("Alarm triggered with high frequency");
             tone(buzzer_PIN, 2000, 500);
--- esp32_gps_speaker/src/main.cpp
+++ esp32_gps_speaker/src/main.cpp
@@ -2,9 +2,29 @@
 #include <WiFi.h>
 #include "../include/buzzer.hpp"
 #include "../include/wifi.hpp"
-// pins used in the project: 2, 40
+// pins used in the project: 2, 39
 constexpr int LED_PIN = 2;
 
+// buzzer task
+void buzzerTask(void *pvParameters) {
+    while (true) {
+        buzzer::localLoop();
+        vTaskDelay(10 / portTICK_PERIOD_MS); // delay to avoid task blocking
+    }
+}
+
+// other task
+void otherTask(void *pvParameters) {
+    while (true) {
+        // LED keep off if WiFi is connected
+        if (WiFi.status() == WL_CONNECTED) digitalWrite(LED_PIN, LOW);
+        else digitalWrite(LED_PIN, HIGH);
+
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // delay to avoid task blocking
+    }
+}
+
+// setup function
 void setup() 
 {
     Serial.begin(115200);
@@ -12,13 +32,30 @@ void setup()
 
     wifi::localSetup();
     buzzer::localSetup();
+
+    xTaskCreatePinnedToCore(
+        buzzerTask,    // task function
+        "Buzzer Task", // task name
+        10000,         // stack size
+        NULL,          // task parameters
+        1,             // priority
+        NULL,          // task handle
+        1              // run on core 1
+    );
+
+    xTaskCreatePinnedToCore(
+        otherTask,    // task function
+        "Other Task", // task name
+        10000,        // stack size
+        NULL,         // task parameters
+        1,            // priority
+        NULL,         // task handle
+        0             // run on core 0
+    );
 }
 
+// loop function
 void loop() 
 {   
-    // LED keep off if WiFi is connected
-    if (WiFi.status() == WL_CONNECTED) digitalWrite(LED_PIN, LOW);
-    else digitalWrite(LED_PIN, HIGH);
-    
-    buzzer::localLoop();
+    // do nothing since tasks are running
 }
