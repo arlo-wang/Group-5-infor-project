917b322e3f6ef9c917a5f4f142fe9b87f0aee943
OpNUB <78775816+opnuub@users.noreply.github.com>
2025-03-11 13:11:47 +0000

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -48,9 +48,6 @@ REST_FRAMEWORK = {
     'DEFAULT_THROTTLE_CLASSES': [
         'rest_framework.throttling.AnonRateThrottle',
     ],
-    'DEFAULT_THROTTLE_RATES': {
-        'anon': '10/min',
-    },
 }
 
 from datetime import timedelta
